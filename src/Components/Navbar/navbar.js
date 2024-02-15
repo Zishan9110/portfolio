17e1a480c25097956e761fed8e2dd0f1e496795d
zishan9110 <zishanshams44@gmail.com>
2024-02-15 23:02:34 +0530

    dffggfd

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="navbar">
         <img src={logo} alt="logo" className='logo'/>
         <div className="desktopMenu">
-        <Link className="desktopMenuListItem">Home</Link>
+        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} className="desktopMenuListItem">Home</Link>
         <Link className="desktopMenuListItem">Client</Link>
         <Link className="desktopMenuListItem">Portfoliot</Link>
         <Link className="desktopMenuListItem">About</Link>
